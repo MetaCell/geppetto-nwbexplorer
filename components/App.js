4eeee1b09449f794a5bacd8669be1da1c9f6bf4a
rodriguez-facundo <bruseghini.f@gmail.com>
2019-08-19 19:38:30 -0300

    Fix replace for undefined

--- components/App.js
+++ components/App.js
@@ -38,9 +38,9 @@ export default class App extends React.Component{
     self = this;
     
     const cookieName = 'nwbloadurl'
-    const nwbCookie = document.cookie.split(';').find(cookie => cookie.includes(cookieName)).replace(/"/g, '')
+    const nwbCookie = document.cookie.split(';').find(cookie => cookie.includes(cookieName))
     if (nwbCookie) {
-      const [_, nwbFileUrl] = nwbCookie.split("=")
+      const [_, nwbFileUrl] = nwbCookie.replace(/"/g, '').split("=")
       if (nwbFileUrl && document.location.href.includes("nwbexplorer.opensourcebrain.org")) {
         document.cookie = `${cookieName}= ; path=/`
         document.location.href = `${document.location.href}?nwbfile=${nwbFileUrl}`;
